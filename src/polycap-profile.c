0e0cb66dc23007fa687430298a240a0e3881ecb7
Pieter Tack <pieter.tack@UGent.be>
2018-01-23 11:44:38 +0100

    Combine files

---
+++ src/polycap-profile.c
@@ -0,0 +1,61 @@
+// get a new profile for a given type with properties
+polycap_profile* polycap_profile_new(
+	enum polycap_profile_type type,
+	double length,
+	double rad_ext[2],
+	double rad_int[2],
+	double focal_dist[2])
+{
+	struct cap_prof_arrays *shape_arr;
+
+	shape_arr = def_cap_profile(enum polycap_profile_type type, double length, double rad_ext[2], double rad_int[2], double focal_dist[2]);
+
+	return shape_arr;
+
+}
+//===========================================
+// get a new profile from Laszlo's ASCII files
+polycap_profile* polycap_profile_new_from_file(
+	const char *single_cap_profile_file,
+	const char *central_axis_file,
+	const char *external_shape_file)
+{
+	struct cap_prof_arrays *shape_arr;
+	struct inp_file *cap;
+	struct cap_profile *profile;
+
+	//duplicate file names to structure as accepted by read_cap_profile
+	cap->prf = strdup(single_cap_profile_file);
+	cap->axs = strdup(central_axis_file);
+	cap->ext = strdup(external_shape_file);
+
+	//create profile struct for output as made by read_cap_profile
+	profile = malloc(sizeof(struct cap_profile));
+
+	//read files
+	read_cap_profile(cap, profile);
+
+	//transfer shape information to structure with identical output as polycap_profile_new()
+	shape_arr = malloc(sizeof(struct cap_prof_arrays)*(profile->nmax+1));
+	for(i=0;i<=profile->nmax;i++){
+		shape_arr[i].zarr = profile->arr[i].zarr;
+		shape_arr[i].profil = profile->arr[i].profil;
+		shape_arr[i].d_arr = profile->arr[i].d_arr;
+	}
+
+	free(cap);
+	free(profile->arr);
+	free(profile);
+
+	return shape_arr;
+
+}
+//===========================================
+// free the polycap_profile structure and its associated data
+void polycap_profile_free(polycap_profile *profile)
+{
+
+	free(profile);
+
+}
+
