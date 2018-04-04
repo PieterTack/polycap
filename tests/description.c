e31aecb5c8c9c8f6cae36b898d5971d618947fee
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-04-04 13:50:22 +0100

    Fix tests/Makefile.am

--- tests/description.c
+++ tests/description.c
@@ -59,7 +59,7 @@ void test_polycap_description_new_from_file() {
 	//test with example file
 	//TODO: fix the file path name so it won't fail on other computers... (e.g. where is this example file stored?)
 	polycap_clear_error(&error);
-	description = polycap_description_new_from_file("/home/ptack/poly_raytrace/example/ellip_l9.inp", &source, &error);
+	description = polycap_description_new_from_file(EXAMPLE_DIR "ellip_l9.inp", &source, &error);
 	assert(description != NULL);
 }
 
