f2222fc03bc6a72f8a7135b42c64d464b80a837c
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-04-20 08:49:33 +0100

    AppVeyor: fixes

--- appveyor-before-build.sh
+++ appveyor-before-build.sh
@@ -6,19 +6,22 @@ set -x
 export PKG_CONFIG_PATH=$HOME/install/lib/pkgconfig
 export PATH=$HOME/install/bin:$PATH
 
+XRL_VERSION=4.0.0
+EASYRNG_VERSION=1.2
+
 # install xraylib
-wget -q https://xraylib.tomschoonjans.eu/xraylib-3.3.0.tar.gz
-tar xfz xraylib-3.3.0.tar.gz
-cd xraylib-3.3.0
+wget -q https://xraylib.tomschoonjans.eu/xraylib-${XRL_VERSION}.tar.gz
+tar xfz xraylib-${XRL_VERSION}.tar.gz
+cd xraylib-${XRL_VERSION}
 ./configure --prefix=$HOME/install --disable-static
 make -j2
 make install
 cd ..
 
 if test $RNG = "easyRNG" ; then
-	wget -q https://github.com/tschoonj/easyRNG/releases/download/easyRNG-1.1/easyRNG-1.1.tar.gz
-	tar xfz easyRNG-1.1.tar.gz
-	cd easyRNG-1.1
+	wget -q https://github.com/tschoonj/easyRNG/releases/download/easyRNG-${EASYRNG_VERSION}/easyRNG-${EASYRNG_VERSION}.tar.gz
+	tar xfz easyRNG-${EASYRNG_VERSION}.tar.gz
+	cd easyRNG-${EASYRNG_VERSION}
 	./configure --prefix=$HOME/install --disable-static
 	make -j2
 	make install
--- appveyor-build-script.sh
+++ appveyor-build-script.sh
@@ -5,30 +5,14 @@ set -x
 
 export PKG_CONFIG_PATH=$HOME/install/lib/pkgconfig
 export PATH=$HOME/install/bin:$PATH
-export CYTHON=cython2
+export CYTHON=cython
 export HDF5_CFLAGS="-I/mingw64/include"
 export HDF5_LIBS="-L/mingw64/lib -lhdf5"
 
 cd $APPVEYOR_BUILD_FOLDER
 
 autoreconf -fi
-export CFLAGS="-Wall -Werror"
-./configure --prefix=$HOME/install --disable-python
-make
-make check
-make distcheck
-make distclean
-
-export PYTHON=python2
-./configure --prefix=$HOME/install --enable-python
-make
-make check
-make distcheck
-make distclean
-
 export PYTHON=python3
 ./configure --prefix=$HOME/install --enable-python
 make
 make check
-make distcheck
-make distclean
