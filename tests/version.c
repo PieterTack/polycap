820165a3f7d258beee1c2d2515969235270acf4f
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-06-03 09:35:03 +0100

    tests/version: fix segfault

--- tests/version.c
+++ tests/version.c
@@ -4,8 +4,8 @@
 
 int main(int argc, char **argv){
 
-	//gchar **header_version = g_strdup_printf("%d.%d", POLYCAP_VERSION_MAJOR, POLYCAP_VERSION_MINOR);
-	char **header_version = vasprintf("%d.%d", POLYCAP_VERSION_MAJOR, POLYCAP_VERSION_MINOR);
+	char *header_version = NULL;
+	asprintf(&header_version, "%d.%d", POLYCAP_VERSION_MAJOR, POLYCAP_VERSION_MINOR);
 	if(strcmp(header_version, PACKAGE_VERSION) == 0){
 		return 0;
 	}
